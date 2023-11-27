aec36ff42eeb2d0465e1cd8d5902dd39d93871b5
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-11-27 11:24:37 -0500

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+    return data;
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+main();
+
+}
+    const data = [];
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+}
+    });
+
+    }
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+
+    for item in data:
+    return data
+import random
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
